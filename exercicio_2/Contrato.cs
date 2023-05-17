958e657f69de0ae9e36af27b0a6a5588a7c89d1f
Bianca Prado <100374298+projectspradobianca@users.noreply.github.com>
2023-05-17 14:44:13 -0300

    Add files via upload

---
+++ exercicio_2/Contrato.cs
@@ -0,0 +1,19 @@
+﻿namespace exercicio_2;
+class Contrato
+{
+   public string? nome {get;set;}
+
+   public virtual string Email(){
+    return "Email: ";
+   }
+
+   public virtual string Telefone(){
+    return "Telefone: ";
+
+    public string MostraDados(){
+        console.Writeline("Nome: "+ this.nome);
+        console.Writeline("Email: "+this.Email);
+        console.Writeline("Telefone: "+this.Telefone);
+    }
+   }
+}
---
+++ exercicio_2/PessoaFisica.cs
@@ -0,0 +1,17 @@
+﻿namespace exercicio_2;
+class PessoaFisica:Contrato
+{
+   public string Cpf(){
+    return "666.666.666-66"
+   }
+
+   public int Idade(){
+    return "Idade: ";
+   }
+
+   public virtual string Email(){
+    return base.Email()+"camposbiancapp@gmail.com";
+   }
+
+
+}
